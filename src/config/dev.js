5cd5b89e9303e45c31db55b2c1d4c3b91a7603e4
Sergio Franco <checho1536@gmail.com>
2022-04-25 10:34:56 -0500

    gitignore is working

--- src/config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    postgres: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgresDbBoda',
-        database: 'boda',
-        port:'5432'
-    }
-}
